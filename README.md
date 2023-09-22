11d5e69d3866ed686dbb3540e1f9d73b777daa44
JoelKanyi <joelkanyi98@gmail.com>
2023-09-22 13:27:47 +0300

    bump to 1.0.5

---
+++ README.md
@@ -0,0 +1,1 @@
+Here is the [sample](https://github.com/JoelKanyi/KomposePickerSample) project.
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,5 +66,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("com.github.JoelKanyi:KomposeCountryCodePicker:1.0.3")
+    implementation("com.github.JoelKanyi:KomposeCountryCodePicker:1.0.5")
 }
